669ed9a822cc17e6cf8b9acc4fb038c3bf747001
Paulo Sergio Sampaio de Aragao <paulo.aragao@gmail.com>
2019-09-09 07:46:27 +0200

    no message

--- src/featureInfo/ImageryLayerFeatureInfo.js
+++ src/featureInfo/ImageryLayerFeatureInfo.js
@@ -43,11 +43,8 @@ Ext.define('CesiumExt.featureInfo.ImageryLayerFeatureInfo', {
 		var me = this;
 		if(!config.viewer)
 			Ext.raise('viewer must be provided.');
-		me.viewer = config.viewer;
 		if(!config.imageryLayers)
 			Ext.raise('imageryLayers must be provided.');
-		else
-		//delete config.imageryLayers;
 		if(!config.message)
 			config.message = me.config.message;
 		me.initConfig(config);
@@ -55,13 +52,17 @@ Ext.define('CesiumExt.featureInfo.ImageryLayerFeatureInfo', {
 		console.log(me.viewer);
     },
 	
+	/**
+	* Method to retrieve the feature information from the imagery layer based
+	* on the position indicated by the user through the mouse click in the canvas
+	**/
 	getFeatures: function()
 	{
 		var me = this;
 		//create interaction to get position
 		var getPosInteraction = Ext.create('CesiumExt.interaction.GetPositionInteraction',
 		{
-			viewer: me.viewer,
+			viewer: me.getViewer(),
 			message: me.getMessage()
 		});
 		
@@ -74,7 +75,58 @@ Ext.define('CesiumExt.featureInfo.ImageryLayerFeatureInfo', {
 				var me = context;
 				var windowPos = data.windowPosition;
 				
+				//create panel
+				var accordion = Ext.create('Ext.Panel', {
+					layout: 'accordion',
+					items: []
+				});
+				//create and show popup window
+				var popupWindow = Ext.create('Ext.window.Window', {
+					title: "Feature Info",
+					height: 600,
+					width: 450,
+					scrollable: true,
+					constrainHeader: true,
+					items: [accordion]
+				});
+				popupWindow.show();
+				
+				var pickRay = me.getViewer().camera.getPickRay(windowPos);
+				var featuresPromise = me.getViewer().imageryLayers.pickImageryLayerFeatures(pickRay, me.getViewer().scene);
+				
+				
+				if (!Cesium.defined(featuresPromise)) {
+					console.log('No features picked.');
+				}
+				else {
+					Cesium.when(featuresPromise, function(features) {
+						console.log('Number of features: ' + features.length);
+						if (features.length > 0) {
+							console.log('First feature properties: ' + features[0].properties);
+							console.log('First feature layer: ' +  features[0].imageryLayer.imageryProvider.layers);
+							
+							
+							
+						
+							//iterate over all the features to show the properties for each feature
+							Ext.each(features, function(feature) {
+								var featGrid = Ext.create('Ext.grid.property.Grid', {
+									title: feature.imageryLayer.imageryProvider.layers,
+									source: feature.properties,
+									sortableColumns: false,
+								});
+								accordion.add(featGrid);
+							});
+							popupWindow.items.getAt(0).updateLayout();
+						}
+						else {
+							Ext.Msg.alert('Message', 'No feature found!!!', Ext.emptyFn);
+						}
+					});
+				}
+				
 				getPosInteraction.un('positionRetrieved', showFeatureInfoBox);
+				getPosInteraction.destroy();
 				console.log('FeatureInfoBox:' + windowPos);
 			}
 		});
